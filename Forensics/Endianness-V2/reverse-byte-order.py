a86cd78235306f5451268fec6c349dd561fdc3fd
Rogue Blue <rogueblueprint@protonmail.com>
2024-07-07 23:35:58 -0700

    Finished walkthrough for endiannessv2

--- Forensics/Endianness-V2/reverse-byte-order.py
+++ Forensics/Endianness-V2/reverse-byte-order.py
@@ -1,14 +1,28 @@
+# Grab chunks in 4 byte sizes
 chunk_size = 4
 
-f = open("challengefile", mode="rb")
+# Open challengefile in read bytes mode
+f1 = open("challengefile", mode="rb")
+
+# Open challengefilereversed in write bytes mode
 f2 = open("challengefilereversed", mode="wb")
 
+# Repeat until there are no chunks being read
 while True:
-    chunk = f.read(chunk_size)
+
+    # Read chunk_size byes from file and store them in chunk variable
+    chunk = f1.read(chunk_size)
+
+    # No more chunks, end loop
     if not chunk:
         break
+
+    # Reverse bytes and store them in bytes_reversed
     bytes_reversed = chunk[::-1]
+
+    # Write reversed bytes to challengefilereversed file
     f2.write(bytes_reversed)
 
-f.close()
+# Close open file handles
+f1.close()
 f2.close()
